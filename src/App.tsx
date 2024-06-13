05eac9ee50e62a89976785e519e7db3e8323e427
Yana lab <yanagithuby@gmail.com>
2024-06-13 22:13:09 +0300

    уолулцоа

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,32 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { Provider } from 'react-redux';
+import { isMobile } from 'react-device-detect';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import HomePage from './Pages/HomePage';
 import FriendsPage from './Pages/FriendsPage';
 import TasksPage from './Pages/TasksPage';
-import { store } from './redux-store/store';
 
 const App: React.FC = () => {
   return (
-    <Provider store={store}>
-      <Router>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/friends" element={<FriendsPage />} />
-          <Route path="/tasks" element={<TasksPage />} />
-        </Routes>
-      </Router>
-    </Provider>
+    <Router>
+      <Routes>
+      <Route path='/:userId/:username' element={<HomePage />} />
+        <Route path="/friends/:userId/:username" element={<FriendsPage />} />
+        <Route path="/tasks/:userId/:username" element={<TasksPage />} />
+        {/* <Route path='/' element={<HomePage />} />
+        <Route path="/friends" element={<FriendsPage />} />
+        <Route path="/tasks" element={<TasksPage />} /> */}
+        {/* <Route path="/" element={!isMobile ? (
+          <div className="App">
+            <header className="App-header">
+              <p>Откройте приложение на вашем смартфоне</p>
+            </header>
+          </div>
+        ) : (
+          <Navigate to="/home" />
+        )} /> */}
+      </Routes>
+    </Router>
   );
-};
+}
 
 export default App;
